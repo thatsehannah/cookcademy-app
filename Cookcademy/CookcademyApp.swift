7299af0a16eaabf055d819ceb1aea0a0f7193c79
E. Hannah <echannah631@gmail.com>
2023-03-21 17:52:54 -0500

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Elliot Hannah III on 3/21/23.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
