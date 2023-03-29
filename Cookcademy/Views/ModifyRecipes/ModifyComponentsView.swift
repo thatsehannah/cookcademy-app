b60d53c51e187be49b44467616900b8b8bfe7cc6
E. Hannah <echannah631@gmail.com>
2023-03-29 11:33:05 -0500

    refactored modifycomponentview to display the generic component information (instead of ingredient)

--- Cookcademy/Views/ModifyRecipes/ModifyComponentsView.swift
+++ Cookcademy/Views/ModifyRecipes/ModifyComponentsView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-protocol RecipeComponent {
+protocol RecipeComponent: CustomStringConvertible {
     init()
 }
 
@@ -22,31 +22,31 @@ protocol ModifyComponentView: View {
     init(component: Binding<Component>, createAction: @escaping (Component) -> Void)
 }
 
-struct ModifyComponentsView: View {
-    @Binding var ingredients: [Ingredient]
-    @State private var newIngredient = Ingredient()
+struct ModifyComponentsView<Component: RecipeComponent, DestinationView: ModifyComponentView>: View where DestinationView.Component == Component {
+    @Binding var components: [Component]
+    @State private var newComponent = Component()
     
     private let listBackgroundColor = AppColor.background
     private let listTextColor = AppColor.foreground
     
     var body: some View {
         VStack {
-            let addIngredientView = ModifyIngredientView(component: $newIngredient) { ingredient in
-                ingredients.append(ingredient)
-                newIngredient = Ingredient()
-            }.navigationTitle("Add Ingredient")
-            if ingredients.isEmpty {
+            let addComponentView = DestinationView(component: $newComponent) { component in
+                components.append(component)
+                newComponent = Component()
+            }.navigationTitle("Add Component")
+            if components.isEmpty {
                 Spacer()
-                NavigationLink("Add the first ingredient", destination: addIngredientView)
+                NavigationLink("Add the first component", destination: addComponentView)
                 Spacer()
             } else {
                 List {
-                    ForEach(ingredients.indices, id: \.self) { index in
-                        let ingredient = ingredients[index]
-                        Text(ingredient.description)
+                    ForEach(components.indices, id: \.self) { index in
+                        let component = components[index]
+                        Text(component.description)
                     }
                     .listRowBackground(listBackgroundColor)
-                    NavigationLink("Add another ingredient", destination: addIngredientView)
+                    NavigationLink("Add another component", destination: addComponentView)
                     .buttonStyle(PlainButtonStyle())
                     .listRowBackground(listBackgroundColor)
                 }
@@ -57,11 +57,12 @@ struct ModifyComponentsView: View {
 }
 
 struct ModifyMultipleIngredientsView_Previews: PreviewProvider {
+    @State static var recipe = Recipe.testRecipes[1]
     @State static var emptyIngredients = [Ingredient]()
     
     static var previews: some View {
         NavigationView {
-            ModifyComponentsView(ingredients: $emptyIngredients)
+            ModifyComponentsView<Ingredient, ModifyIngredientView>(components: $recipe.ingredients)
         }
         
     }
